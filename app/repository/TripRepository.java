8e7b69ca7ff12475db2662ba555177a3d3c9097c
Jade <jma361@uclive.ac.nz>
2019-03-14 14:50:32 +1300

    added trip respository to the repo#story[218]

--- app/repository/TripRepository.java
+++ app/repository/TripRepository.java
@@ -1,4 +1,31 @@
 package repository;
 
+import io.ebean.Ebean;
+import io.ebean.EbeanServer;
+import models.Trip;
+import play.db.ebean.EbeanConfig;
+
+import javax.inject.Inject;
+import java.util.Optional;
+import java.util.concurrent.CompletionStage;
+
 public class TripRepository {
+
+    private final EbeanServer ebeanServer;
+    private final DatabaseExecutionContext executionContext;
+
+    @Inject
+    public TripRepository(EbeanConfig ebeanConfig, DatabaseExecutionContext executionContext) {
+        this.ebeanServer = Ebean.getServer(ebeanConfig.defaultServer());
+        this.executionContext = executionContext;
+    }
+
+    public CompletionStage<String> insert(Trip trip) {
+
+    }
+
+    public CompletionStage<Optional<String>> delete(int tripID) {
+
+    }
+
 }
