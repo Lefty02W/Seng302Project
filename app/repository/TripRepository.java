37a3d6d56cf6428745f684f3fb1b5472521d934d
lgg24 <lgg24@uclive.ac.nz>
2019-03-14 16:06:50 +1300

    Implemented uploading new trips to the database and deleting trips from the database

--- app/repository/TripRepository.java
+++ app/repository/TripRepository.java
@@ -1,14 +1,16 @@
 package repository;
 
+import io.ebean.*;
 import io.ebean.Ebean;
 import io.ebean.EbeanServer;
 import models.Trip;
 import play.db.ebean.EbeanConfig;
 import scala.None;
-
 import javax.inject.Inject;
 import java.util.Optional;
 import java.util.concurrent.CompletionStage;
+
+import static java.util.concurrent.CompletableFuture.supplyAsync;
 
 public class TripRepository {
 
@@ -21,14 +23,24 @@ public class TripRepository {
         this.executionContext = executionContext;
     }
 
-    /**
-    public CompletionStage<String> insert(Trip trip) {
 
-
+    public CompletionStage<Integer> insert(Trip trip) {
+        return supplyAsync(() -> {
+            ebeanServer.insert(trip);
+            return trip.getId();
+        }, executionContext);
     }
 
-    public CompletionStage<Optional<String>> delete(int tripID) {
-
-    }*/
+    public CompletionStage<Optional<Integer>> delete(int tripID) {
+        return supplyAsync(() -> {
+            try {
+                final Optional<Trip> tripOptional = Optional.ofNullable(ebeanServer.find(Trip.class).setId(tripID).findOne());
+                tripOptional.ifPresent(Model::delete);
+                return tripOptional.map(p -> p.getId());
+            } catch (Exception e) {
+                return Optional.empty();
+            }
+        }, executionContext);
+    }
 
 }
