f2aecf916c7e68043c36b3548fe5f5700d31c5a6
Luke Walsh <lwa383@uclive.ac.nz>
2019-03-15 16:44:48 +1300

    #fix up redundant code for code review, #chore tidy up javadoc run through the code to make sure ac's are met and code follows nice style #pair[lwa383,cll62] #story[217]

--- app/models/PartnerFormData.java
+++ app/models/PartnerFormData.java
@@ -1,5 +1,8 @@
 package models;
 
+/**
+ * Class for getting form inputs for search functionality of Find a traveller partner story
+ */
 public class PartnerFormData {
     public String searchNationality = "";
     public String searchGender = "";
