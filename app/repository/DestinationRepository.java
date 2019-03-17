2e3c0770ad270042ec160700ffbc640cb0f9e307
Sam Verdellen <sgv15@uclive.ac.nz>
2019-03-18 10:59:01 +1300

    Added new function for editing destinations, just needs to be linked to the UI. #impliment #story[213]

--- app/repository/DestinationRepository.java
+++ app/repository/DestinationRepository.java
@@ -3,6 +3,7 @@ package repository;
 import io.ebean.Ebean;
 import io.ebean.EbeanServer;
 import io.ebean.Model;
+import io.ebean.Transaction;
 import models.Destination;
 import play.db.ebean.EbeanConfig;
 
@@ -51,6 +52,31 @@ public class                                                                    
             } catch (Exception e) {
                 return Optional.empty();
             }
+        }, executionContext);
+    }
+
+    public CompletionStage<Optional<String>> update(Destination newDestination, Integer Id) {
+        return supplyAsync(() -> {
+            Transaction txn = ebeanServer.beginTransaction();
+            Optional<String> value = Optional.empty();
+            try {
+                Destination targetDestination = ebeanServer.find(Destination.class).setId(Id).findOne();
+                if (targetDestination != null) {
+                    targetDestination.setUserEmail(newDestination.getUserEmail());
+                    targetDestination.setName(newDestination.getName());
+                    targetDestination.setType(newDestination.getType());
+                    targetDestination.setCountry(newDestination.getCountry());
+                    targetDestination.setDistrict(newDestination.getDistrict());
+                    targetDestination.setLatitude(newDestination.getLatitude());
+                    targetDestination.setLongitude(newDestination.getLongitude());
+                    targetDestination.update();
+                    txn.commit();
+                    value = Optional.of(String.format("Destination %s edited", newDestination.getDestinationId()));
+                }
+            } finally {
+                txn.end();
+            }
+            return value;
         }, executionContext);
     }
 
