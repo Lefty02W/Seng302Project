644d1c3791bbaba149a2dfe376d4e93156a74d95
Sam Verdellen <sgv15@uclive.ac.nz>
2019-03-24 14:46:40 +1300

    Fixed issue with creating an account with no traveller type #chore

--- app/controllers/CreateUserController.java
+++ app/controllers/CreateUserController.java
@@ -37,7 +37,7 @@ public class CreateUserController extends Controller{
     public Result save(Http.Request request){
         Form<Profile> userForm = form.bindFromRequest(request);
         System.out.println(userForm);
-        Profile profile = userForm.get();
+        Profile profile = userForm.value().get();
         profileRepository.insert(profile);
         return redirect(routes.LoginController.show());
     }
