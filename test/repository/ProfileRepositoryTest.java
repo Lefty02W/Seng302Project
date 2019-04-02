8555866c2d468dd95dffd0c9a5a49753cdf7f031
Ambrose Ledbrook <ajl190@uclive.ac.nz>
2019-04-02 22:57:12 +1300

    Fix first profileRepository test and implement more on the validate method
    #story[287]

--- test/repository/ProfileRepositoryTest.java
+++ test/repository/ProfileRepositoryTest.java
@@ -8,6 +8,8 @@ import play.Application;
 
 public class ProfileRepositoryTest extends ProvideApplication {
 
+    //TODO: write methods to populate db with some test data and then remove it afterwards
+
     private Application app;
 
     @Before
@@ -15,10 +17,64 @@ public class ProfileRepositoryTest extends ProvideApplication {
         app = super.provideApplication();
     }
 
+    /**
+     * Testing the checkProfileExists method with an email that does not exist
+     */
     @Test
     public void checkProfileExistsProfileDoesNotExist() {
         ProfileRepository profileRepository = app.injector().instanceOf(ProfileRepository.class);
         boolean profileExists = profileRepository.checkProfileExists("kdlhafalkjdfhalkdf");
-        Assert.assertNull(profileExists);
+        Assert.assertFalse(profileExists);
+    }
+
+    /**
+     * Testing the checkProfileExists method with an email that does exist
+     */
+    @Test
+    public void checkProfileExistsProfileDoesExist() {
+        ProfileRepository profileRepository = app.injector().instanceOf(ProfileRepository.class);
+        boolean profileExists = profileRepository.checkProfileExists("admin");
+        Assert.assertFalse(profileExists);
+    }
+
+
+    /**
+     * Testing the validate method with invalid email and password
+     */
+    @Test
+    public void validateInvalidEmailAndPassword() {
+        ProfileRepository profileRepository = app.injector().instanceOf(ProfileRepository.class);
+        boolean isValid = profileRepository.validate("sadasdsad", "dklfdhslkf");
+        Assert.assertFalse(isValid);
+    }
+
+    /**
+     * Testing the validate method with invalid password
+     */
+    @Test
+    public void validateInvalidPassword() {
+        ProfileRepository profileRepository = app.injector().instanceOf(ProfileRepository.class);
+        boolean isValid = profileRepository.validate("admin", "sadasdsadsad");
+        Assert.assertFalse(isValid);
+    }
+
+    /**
+     * Testing the validate method with invalid email
+     */
+    @Test
+    public void validateInvalidEmail() {
+        ProfileRepository profileRepository = app.injector().instanceOf(ProfileRepository.class);
+        boolean isValid = profileRepository.validate("adsadasdsadmin", "admin123");
+        Assert.assertFalse(isValid);
+    }
+
+    /**
+     * Testing the validate method with valid email and password
+     */
+    @Test
+    public void validateValidData() {
+        ProfileRepository profileRepository = app.injector().instanceOf(ProfileRepository.class);
+        boolean isValid = profileRepository.validate("admin", "admin123");
+        Assert.assertTrue(isValid);
     }
 }
