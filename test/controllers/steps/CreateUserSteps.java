4237dc55af1cd18285a032df425c40e1a469747a
Luke Walsh <lwa383@uclive.ac.nz>
2019-04-04 17:20:50 +1300

    gherkin tests running and passing for login in redirect on correct login and on incorrect email or password #test #chore #story[287]

--- test/controllers/steps/CreateUserSteps.java
+++ test/controllers/steps/CreateUserSteps.java
@@ -1,193 +1,193 @@
-package controllers.steps;
-
-
-import cucumber.api.java.After;
-import cucumber.api.java.AfterStep;
-import cucumber.api.java.Before;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
-import cucumber.api.java.en.And;
-import org.junit.Assert;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import play.Application;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-import play.test.Helpers;
-import play.test.TestServer;
-import play.test.WithBrowser;
-import static play.test.Helpers.*;
-
-
-/**
- * Implements steps for testing CreateUser
- */
-public class CreateUserSteps extends WithBrowser {
-
-    protected Application application;
-    private WebDriver driver;
-    private WebElement element;
-    private TestServer testServer;
-
-    private final int port = 50000; // Port to use, must not conflict
-    private final String loginPage = "http://localhost:" + port + "/";
-
-    @Before
-    /**
-     * Create an application instance, empty profile, profile form
-     * and run the application.
-     */
-    public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        application = fakeApplication();         // Create a fake application instance
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("headless");
-        driver = new ChromeDriver(options);      // Use Chrome
-        testServer = Helpers.testServer(port, application);
-        testServer.start();  //Run the application
-        driver.manage().window().maximize();
-    }
-
-
-    @AfterStep
-    // Simply to allow visual following of selenium's execution
-    public void pause() throws InterruptedException {
-        Thread.sleep(500); // 0.5 second delay
-    }
-
-
-    @After
-    public void teardown() {
-        driver.close();
-        driver.quit();
-        testServer.application().stop();
-        testServer.stop();
-    }
-
-
-    @Given("John is at the sign up page")
-    public void at_sign_up_page() throws InterruptedException {
-        driver.get(loginPage);
-        element = driver.findElement(By.id("createUserButton"));
-        element.click();
-        element = driver.findElement(By.id("createProfileModal"));
-
-        Assert.assertNotNull(element);
-    }
-
-
-    @When("he fills in First Name with {string}")
-    public void fill_first_name(String firstName) {
-        element = driver.findElement(By.id("firstName"));
-        element.click();
-        element.sendKeys(firstName);
-        Assert.assertEquals(firstName, element.getAttribute("value"));
-    }
-
-
-    @And("he fills in Middle Name with {string}")
-    public void fill_middle_name(String middleName) { ;
-        element = driver.findElement(By.id("middleName"));
-        element.sendKeys(middleName);
-
-        Assert.assertEquals(middleName, element.getAttribute("value"));
-    }
-
-
-    @And("he fills in Last Name with {string}")
-    public void fill_last_name(String lastName) {
-        element = driver.findElement(By.id("lastName"));
-        element.sendKeys(lastName);
-        Assert.assertEquals(lastName, element.getAttribute("value"));
-    }
-
-
-    @And("he fills in Email with {string}")
-    public void fill_email(String email) {
-        element = driver.findElement(By.className("createEmail"));
-        element.sendKeys(email);
-        Assert.assertEquals(email, element.getAttribute("value"));
-    }
-
-
-    @And("he fills in Password with {string}")
-    public void fill_password(String password) {
-        element = driver.findElement(By.className("createPassword"));
-        element.sendKeys(password);
-        Assert.assertEquals(password, element.getAttribute("value"));
-    }
-
-
-    @And("he fills in Gender with {string}")
-    public void fill_gender(String gender) {
-        Select genderSelect = new Select(driver.findElement(By.id("gender")));
-        genderSelect.selectByVisibleText(gender);
-
-        Assert.assertEquals(gender, genderSelect.getFirstSelectedOption().getAttribute("value"));
-    }
-
-
-    @And("he fills in Birth date with {string}")
-    public void fill_birth_date(String birthDate) throws ParseException {
-        element = driver.findElement(By.id("birthDate"));
-        element.sendKeys(birthDate);
-
-        Assert.assertNotNull(element.getAttribute("value"));
-    }
-
-
-    @And("he fills in Nationalities with {string}")
-    public void fill_nationality(String nationality) {
-        element = driver.findElement(By.id("nationalities"));
-        element.sendKeys(nationality);
-
-        Assert.assertEquals(nationality, element.getAttribute("value"));
-    }
-
-
-    @And("he fills in Passport with {string}")
-    public void fill_passport(String passport) {
-        element = driver.findElement(By.id("passports"));
-        element.sendKeys(passport);
-
-        Assert.assertEquals(passport, element.getAttribute("value"));
-    }
-
-
-    @And("he selects {string} from Traveller Type")
-    public void select_traveller_type(String type) {
-        Select travellerTypes = new Select(driver.findElement(By.id("typeDropdown")));
-        travellerTypes.selectByValue(type);
-
-        Assert.assertEquals(type, travellerTypes.getFirstSelectedOption().getAttribute("value"));
-    }
-
-
-    @And("he presses OK")
-    public void press_ok() {
-        element = driver.findElement(By.id("createButton"));
-        element.click();
-
-        Assert.assertEquals(driver.getCurrentUrl(), loginPage);
-    }
-
-
-    @Then("the login page should be shown")
-    public void login() {
-        // Ensure sign up modal is no longer on page
-        Assert.assertEquals(0, driver.findElements(By.id("createUserModal")).size());
-    }
-}
+//package controllers.steps;
+//
+//
+//import cucumber.api.java.After;
+//import cucumber.api.java.AfterStep;
+//import cucumber.api.java.Before;
+//import cucumber.api.java.en.Given;
+//import cucumber.api.java.en.Then;
+//import cucumber.api.java.en.When;
+//import cucumber.api.java.en.And;
+//import org.junit.Assert;
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.chrome.ChromeOptions;
+//import org.openqa.selenium.support.ui.Select;
+//import org.openqa.selenium.support.ui.WebDriverWait;
+//import play.Application;
+//
+//import java.text.ParseException;
+//import java.text.SimpleDateFormat;
+//import java.time.LocalDate;
+//import java.time.format.DateTimeFormatter;
+//import java.util.Date;
+//
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//
+//import io.github.bonigarcia.wdm.WebDriverManager;
+//import play.test.Helpers;
+//import play.test.TestServer;
+//import play.test.WithBrowser;
+//import static play.test.Helpers.*;
+//
+//
+///**
+// * Implements steps for testing CreateUser
+// */
+//public class CreateUserSteps extends WithBrowser {
+//
+//    protected Application application;
+//    private WebDriver driver;
+//    private WebElement element;
+//    private TestServer testServer;
+//
+//    private final int port = 50000; // Port to use, must not conflict
+//    private final String loginPage = "http://localhost:" + port + "/";
+//
+//    @Before
+//    /**
+//     * Create an application instance, empty profile, profile form
+//     * and run the application.
+//     */
+//    public void setUp() {
+//        WebDriverManager.chromedriver().setup();
+//        application = fakeApplication();         // Create a fake application instance
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("headless");
+//        driver = new ChromeDriver(options);      // Use Chrome
+//        testServer = Helpers.testServer(port, application);
+//        testServer.start();  //Run the application
+//        driver.manage().window().maximize();
+//    }
+//
+//
+//    @AfterStep
+//    // Simply to allow visual following of selenium's execution
+//    public void pause() throws InterruptedException {
+//        Thread.sleep(500); // 0.5 second delay
+//    }
+//
+//
+//    @After
+//    public void teardown() {
+//        driver.close();
+//        driver.quit();
+//        testServer.application().stop();
+//        testServer.stop();
+//    }
+//
+//
+//    @Given("John is at the sign up page")
+//    public void at_sign_up_page() throws InterruptedException {
+//        driver.get(loginPage);
+//        element = driver.findElement(By.id("createUserButton"));
+//        element.click();
+//        element = driver.findElement(By.id("createProfileModal"));
+//
+//        Assert.assertNotNull(element);
+//    }
+//
+//
+//    @When("he fills in First Name with {string}")
+//    public void fill_first_name(String firstName) {
+//        element = driver.findElement(By.id("firstName"));
+//        element.click();
+//        element.sendKeys(firstName);
+//        Assert.assertEquals(firstName, element.getAttribute("value"));
+//    }
+//
+//
+//    @And("he fills in Middle Name with {string}")
+//    public void fill_middle_name(String middleName) { ;
+//        element = driver.findElement(By.id("middleName"));
+//        element.sendKeys(middleName);
+//
+//        Assert.assertEquals(middleName, element.getAttribute("value"));
+//    }
+//
+//
+//    @And("he fills in Last Name with {string}")
+//    public void fill_last_name(String lastName) {
+//        element = driver.findElement(By.id("lastName"));
+//        element.sendKeys(lastName);
+//        Assert.assertEquals(lastName, element.getAttribute("value"));
+//    }
+//
+//
+//    @And("he fills in Email with {string}")
+//    public void fill_email(String email) {
+//        element = driver.findElement(By.className("createEmail"));
+//        element.sendKeys(email);
+//        Assert.assertEquals(email, element.getAttribute("value"));
+//    }
+//
+//
+//    @And("he fills in Password with {string}")
+//    public void fill_password(String password) {
+//        element = driver.findElement(By.className("createPassword"));
+//        element.sendKeys(password);
+//        Assert.assertEquals(password, element.getAttribute("value"));
+//    }
+//
+//
+//    @And("he fills in Gender with {string}")
+//    public void fill_gender(String gender) {
+//        Select genderSelect = new Select(driver.findElement(By.id("gender")));
+//        genderSelect.selectByVisibleText(gender);
+//
+//        Assert.assertEquals(gender, genderSelect.getFirstSelectedOption().getAttribute("value"));
+//    }
+//
+//
+//    @And("he fills in Birth date with {string}")
+//    public void fill_birth_date(String birthDate) throws ParseException {
+//        element = driver.findElement(By.id("birthDate"));
+//        element.sendKeys(birthDate);
+//
+//        Assert.assertNotNull(element.getAttribute("value"));
+//    }
+//
+//
+//    @And("he fills in Nationalities with {string}")
+//    public void fill_nationality(String nationality) {
+//        element = driver.findElement(By.id("nationalities"));
+//        element.sendKeys(nationality);
+//
+//        Assert.assertEquals(nationality, element.getAttribute("value"));
+//    }
+//
+//
+//    @And("he fills in Passport with {string}")
+//    public void fill_passport(String passport) {
+//        element = driver.findElement(By.id("passports"));
+//        element.sendKeys(passport);
+//
+//        Assert.assertEquals(passport, element.getAttribute("value"));
+//    }
+//
+//
+//    @And("he selects {string} from Traveller Type")
+//    public void select_traveller_type(String type) {
+//        Select travellerTypes = new Select(driver.findElement(By.id("typeDropdown")));
+//        travellerTypes.selectByValue(type);
+//
+//        Assert.assertEquals(type, travellerTypes.getFirstSelectedOption().getAttribute("value"));
+//    }
+//
+//
+//    @And("he presses OK")
+//    public void press_ok() {
+//        element = driver.findElement(By.id("createButton"));
+//        element.click();
+//
+//        Assert.assertEquals(driver.getCurrentUrl(), loginPage);
+//    }
+//
+//
+//    @Then("the login page should be shown")
+//    public void login() {
+//        // Ensure sign up modal is no longer on page
+//        Assert.assertEquals(0, driver.findElements(By.id("createUserModal")).size());
+//    }
+//}
--- test/controllers/steps/LoginUserSteps.java
+++ test/controllers/steps/LoginUserSteps.java
@@ -1,124 +1,70 @@
-//package controllers.steps;
-//
-//import controllers.SessionController;
-//import cucumber.api.java.After;
-//import cucumber.api.java.AfterStep;
-//import cucumber.api.java.Before;
-//import cucumber.api.java.en.And;
-//import cucumber.api.java.en.Given;
-//import cucumber.api.java.en.Then;
-//import cucumber.api.java.en.When;
-//import io.github.bonigarcia.wdm.WebDriverManager;
-//import models.Profile;
-//import org.junit.Assert;
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.Cookie;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.chrome.ChromeOptions;
-//import org.openqa.selenium.support.ui.Select;
-//import org.openqa.selenium.support.ui.WebDriverWait;
-//import play.Application;
-//import play.mvc.Http;
-//import play.test.Helpers;
-//import play.test.TestServer;
-//import play.test.WithBrowser;
-//
-//import java.util.Set;
-//
-//import static play.test.Helpers.fakeApplication;
-//
-//public class LoginUserSteps extends WithBrowser {
-//
-//    protected Application application;
-//    private WebDriver driver;
-//    private WebElement element;
-//    private TestServer testServer;
-//
-//    private final int port = 50000; // Port to use, must not conflict
-//    private final String loginPage = "http://localhost:" + port + "/";
-//    private final String profilePage = "http://localhost:" + port + "/profile";
-//
-//    @Before
-//    /**
-//     * Create an application instance, empty profile, profile form
-//     * and run the application.
-//     */
-//    public void setUp() {
-//        WebDriverManager.chromedriver().setup();
-//        application = fakeApplication();         // Create a fake application instance
-//        ChromeOptions options = new ChromeOptions();
-//        //options.addArguments("headless");
-//        driver = new ChromeDriver();      // Use Chrome
-//        testServer = Helpers.testServer(port, application);
-//        testServer.start();  //Run the application
-//        driver.manage().window().maximize();
-//    }
-//
-//
-//    @AfterStep
-//    // Simply to allow visual following of selenium's execution
-//    public void pause() throws InterruptedException {
-//        Thread.sleep(1000); // 0.5 second delay
-//    }
-//
-//
-//    @After
-//    public void teardown() {
-//        driver.close();
-//        driver.quit();
-//        testServer.application().stop();
-//        testServer.stop();
-//    }
-//
-//
-//    @Given("John is at the login page")
-//    public void at_login_page() {
-//        driver.get(loginPage);
-//    }
-//
-//
-//    @When("he fills in his email with {string}")
-//    public void fill_email(String email) {
-//        element = driver.findElement(By.className("loginEmail"));
-//        element.sendKeys(email);
-//        Assert.assertEquals(email, element.getAttribute("value"));
-//    }
-//
-//
-//    @And("he types {string} for his password")
-//    public void fill_password(String password) {
-//        element = driver.findElement(By.id("password"));
-//        element.sendKeys(password);
-//        System.out.print("NOT DUPLICATE");
-//
-//        Assert.assertEquals(password, element.getAttribute("value"));
-//    }
-//
-//
-//    @And("he presses Login")
-//    public void press_ok() {
-//        element = driver.findElement(By.id("loginButton"));
-//        element.click();
-//
-//        Assert.assertEquals(driver.getCurrentUrl(), profilePage);
-//    }
-//
-//
-//    @Then("the profile page should be shown")
-//    public void login() {
-//        String title = driver.getTitle();
-//        // Ensure sign up modal is no longer on page
-//        System.out.print(driver.findElements(By.id("pageTitle")));
-//        Assert.assertEquals(title, driver.getCurrentUrl());
-//    }
-//
-//    @And("Johns session is loaded in")
-//    public void check_session(){
-//        Set<Cookie> cookies = driver.manage().getCookies();
-//        System.out.println(cookies.contains("connected"));
-//
-//
-//    }
-//}
+package controllers.steps;
+
+import controllers.ProvideApplication;
+import controllers.SessionController;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import play.api.mvc.Session;
+import play.mvc.Http;
+import play.mvc.Result;
+import play.test.Helpers;
+import java.util.HashMap;
+import java.util.Map;
+import static org.junit.Assert.*;
+
+public class LoginUserSteps extends ProvideApplication {
+
+    Map<String, String> loginForm = new HashMap<>();
+    Result redirectLoginResult;
+    Result loginResult;
+
+    @Given("John is at the login page")
+    public void johnIsAtTheLoginPage() {
+        Http.RequestBuilder request = Helpers.fakeRequest()
+                .method("GET")
+                .uri("/");
+        redirectLoginResult = Helpers.route(provideApplication(), request);
+
+        assertEquals(200, redirectLoginResult.status());
+
+    }
+
+
+    @When("he fills in his email with {string}")
+    public void heFillsInEmailWith(String string) {
+        loginForm.put("email", string);
+    }
+
+
+    @And("he fills in his password with {string}")
+    public void heFillsInHisPasswordWith(String string) {
+        loginForm.put("password", string);
+    }
+
+
+    @And("he presses Login")
+    public void hePressesLogin() {
+        Http.RequestBuilder request = Helpers.fakeRequest()
+                .method("POST")
+                .uri("/login")
+                .bodyForm(loginForm)
+                .session("connected", "john@gmail.com");
+
+        loginResult = Helpers.route(provideApplication(), request);
+    }
+
+
+    @Then("the profile page should be shown")
+    public void theProfilePageShouldBeShown() {
+        assertEquals(303, loginResult.status());
+        assertEquals("/profile", loginResult.redirectLocation().get());
+    }
+
+    @Then("he is not redirected to the profile page")
+    public void iAmNotRedirectedToTheProfilePage() {
+        assertEquals(303, loginResult.status());
+        assertEquals("/", loginResult.redirectLocation().get());
+    }
+}
