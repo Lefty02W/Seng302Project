ecf9b09e934d1e2374d467a55cae463f7977ddcc
Sam Verdellen <sgv15@uclive.ac.nz>
2019-05-02 15:43:24 +1200

    Added crop details to image class #implement

--- app/models/Image.java
+++ app/models/Image.java
@@ -39,6 +39,18 @@ public class Image extends Model {
     @Constraints.Required
     private String name;
 
+    @Constraints.Required
+    private int cropX;
+
+    @Constraints.Required
+    private int cropY;
+
+    @Constraints.Required
+    private int cropWidth;
+
+    @Constraints.Required
+    private int cropHeight;
+
 
     /**
      * Constructor for image
@@ -105,6 +117,38 @@ public class Image extends Model {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getCropX() {
+        return cropX;
+    }
+
+    public void setCropX(int cropX) {
+        this.cropX = cropX;
+    }
+
+    public int getCropY() {
+        return cropY;
+    }
+
+    public void setCropY(int cropY) {
+        this.cropY = cropY;
+    }
+
+    public int getCropWidth() {
+        return cropWidth;
+    }
+
+    public void setCropWidth(int cropWidth) {
+        this.cropWidth = cropWidth;
+    }
+
+    public int getCropHeight() {
+        return cropHeight;
+    }
+
+    public void setCropHeight(int cropHeight) {
+        this.cropHeight = cropHeight;
     }
 
 
