9f51e18ee228abbb2ccf9eb9e37b1c01853e533d
jma361 <jma361@uclive.ac.nz>
2019-05-02 18:25:05 +1200

    Merged trips into development branch had a few conflicts to fix, re formatted the styling of the heading in the trips page to format correctly aswell. #Story[289] #pair[jma361, lwa383]

--- app/controllers/TripsController.java
+++ app/controllers/TripsController.java
@@ -16,6 +16,7 @@ import play.mvc.Result;
 import play.mvc.Security;
 import repository.TripRepository;
 import views.html.trips;
+import views.html.tripsCard;
 import views.html.tripsCreate;
 import views.html.tripsEdit;
 
@@ -58,7 +59,7 @@ public class TripsController extends Controller {
         orderedCurrentDestinations.clear();
         TreeMultimap<Long, Integer> tripsMap = SessionController.getCurrentUser(request).getTrips();
         List<Integer> tripValues = new ArrayList<>(tripsMap.values());
-        return ok(trips.render(form, destinationsList, tripValues, SessionController.getCurrentUser(request), request, messagesApi.preferred(request)));
+        return ok(tripsCard.render(form, formTrip, destinationsList, tripValues, SessionController.getCurrentUser(request), request, messagesApi.preferred(request)));
     }
 
     /**
