c9e04f9c70215da925d831c288f6e482f86ed042
Luke Walsh <lwa383@uclive.ac.nz>
2019-05-23 20:40:53 +1200

    Choosing file displays in crop window
    -Bind url image to url
    -Call crop window with selected file
    -Image showing and crop slider works on image
    -Added personal photos so images are saved to the same file structure as the vm
    -Need to finish upload/save function
    
    #story[407]

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -96,6 +96,11 @@ create table profile (
   constraint pk_profile primary key (email)
 );
 
+create table traveller_types (
+  traveller_type_id             integer not null,
+  traveller_type_name           varchar(255)
+);
+
 create table trip (
   trip_id                       integer auto_increment not null,
   name                          varchar(255),
@@ -121,6 +126,8 @@ drop table if exists destination;
 drop table if exists image;
 
 drop table if exists profile;
+
+drop table if exists traveller_types;
 
 drop table if exists trip;
 
