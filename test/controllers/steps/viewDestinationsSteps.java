3b3480c394f7dcdd43053840e3fe7da9bead8b13
lgg24 <lgg24@uclive.ac.nz>
2019-05-24 15:47:40 +1200

    implementing a new test to see that a public destination is followed #story[368] #test

---
+++ test/controllers/steps/viewDestinationsSteps.java
@@ -0,0 +1,56 @@
+package controllers.steps;
+
+import controllers.ProvideApplication;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
+import play.filters.csrf.CSRF;
+import play.mvc.Http;
+import play.mvc.Result;
+import play.test.Helpers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class viewDestinationsSteps extends ProvideApplication {
+    private Map<String, String> loginForm = new HashMap<>();
+    private Map<String, String> destForm = new HashMap<>();
+    private Result redirectDestination;
+
+
+    @When("User clicks on public destinations")
+    public void userClicksOnPublicDestinations() {
+        Http.RequestBuilder requestDest = Helpers.fakeRequest()
+                .method("GET")
+                .uri("/destinations/show/true")
+                .session("connected", "1");
+        Result destinationResult = Helpers.route(provideApplication(), requestDest);
+        Assert.assertEquals(200, destinationResult.status());
+    }
+
+    @When("Signs up to a public destination {string}")
+    public void signsUpToAPublicDestination(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("User clicks on private destinations")
+    public void userClicksOnPrivateDestinations() {
+        Http.RequestBuilder requestDest = Helpers.fakeRequest()
+                .method("GET")
+                .uri("/destinations/show/false")
+                .session("connected", "1");
+        Result destinationResult = Helpers.route(provideApplication(), requestDest);
+        Assert.assertEquals(200, destinationResult.status());
+    }
+
+    @Then("Private destinations contains {string}")
+    public void willBeShownInTheirDestinationsList(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+}
