5b0efc00bfe677c0714eed19a967419fe02805f1
Luke Walsh <lwa383@uclive.ac.nz>
2019-05-27 21:32:53 +1200

    Created admin destination create cucumber feature file
    -Wrote 2 scenarios
    -Created Steps file and step methods
    -Need to add functionality to steps
    
    #story[396]

---
+++ test/controllers/steps/AdminCreatesDestinationSteps.java
@@ -0,0 +1,81 @@
+package controllers.steps;
+
+import controllers.ProvideApplication;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class AdminCreatesDestinationSteps extends ProvideApplication {
+
+    @When("admin selects add destination button")
+    public void adminSelectsAddDestinationButton() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("fills the form with correct data")
+    public void fillsTheFormWithCorrectData() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("selects them self as the profile")
+    public void selectsThemSelfAsTheProfile() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the admin presses save")
+    public void theAdminPressesSave() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the new destination is added to the admins destinations")
+    public void theNewDestinationIsAddedToTheAdminsDestinations() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("fills the form with correct data including name as {string}")
+    public void fillsTheFormWithCorrectDataIncludingNameAs(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("selects user {int} as the profile")
+    public void selectsUserAsTheProfile(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the new destination is added to user {int} destinations")
+    public void theNewDestinationIsAddedToUserDestinations(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Given("user is logged in to the application")
+    public void userIsLoggedInToTheApplication() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Given("user with id {string} has a private destination with name {string}, type {string}, and country {string}")
+    public void userWithIdHasAPrivateDestinationWithNameTypeAndCountry(String string, String string2, String string3, String string4) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("user creates a public destination with name {string}, type {string}, and country NewZealand")
+    public void userCreatesAPublicDestinationWithNameTypeAndCountryNewZealand(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Given("user is logged into the application")
+    public void userIsLoggedIntoTheApplication() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+}
---
+++ test/features/AdminCreatesDestination.feature
@@ -0,0 +1,23 @@
+Feature: Create a destination from admin page
+  As an admin
+  I want to be able to create users destinations
+  So I can add new destinations for the users to use
+
+  Scenario: Admin creates a public destination for their user
+    Given Admin is logged in to the application
+    And admin is on the admin page
+    When admin selects add destination button
+    And fills the form with correct data
+    And selects them self as the profile
+    Then the admin presses save
+    And the new destination is added to the admins destinations
+
+
+  Scenario: Admin creates a public destination for another user
+    Given Admin is logged in to the application
+    And admin is on the admin page
+    When admin selects add destination button
+    And fills the form with correct data including name as "adminsTest"
+    And selects user 1 as the profile
+    Then the admin presses save
+    And the new destination is added to user 1 destinations
