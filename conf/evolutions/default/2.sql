7cab3ba87bfadad9e0fd50cc0fa3cc93b6766b5f
George K <gkh36@uclive.ac.nz>
2019-05-28 10:07:55 +1200

    Assert ids in insert statements
    #implement

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -20,19 +20,19 @@ INSERT INTO profile_roles (profile_role_id, profile_id, role_id) values
 
 INSERT INTO profile_roles (profile_id, role_id) values (4, 1);
 
-INSERT INTO destination (profile_id, name, type, country, district, latitude, longitude) values
-  (1, 'Tokyo', 'City', 'Japan', 'Japan', 17.68, 67.98);
+INSERT INTO destination (destination_id, profile_id, name, type, country, district, latitude, longitude) values
+  (1, 1, 'Tokyo', 'City', 'Japan', 'Japan', 17.68, 67.98);
 
-INSERT INTO destination (profile_id, name, type, country, district, latitude, longitude) values
-  (1, 'Matakana', 'Town', 'New Zealand', 'Rodney', 67.45, -67.98);
+INSERT INTO destination (destination_id, profile_id, name, type, country, district, latitude, longitude) values
+  (2, 1, 'Matakana', 'Town', 'New Zealand', 'Rodney', 67.45, -67.98);
 
-INSERT INTO destination (profile_id, name, type, country, district, latitude, longitude, visible) values
-  (3, 'Ruhr', 'Area', 'Germany', 'North Rhine-Westphalia', 51.5, 7.5, 0);
+INSERT INTO destination (destination_id, profile_id, name, type, country, district, latitude, longitude, visible) values
+  (3, 3, 'Ruhr', 'Area', 'Germany', 'North Rhine-Westphalia', 51.5, 7.5, 0);
 
-INSERT INTO destination (profile_id, name, type, country, district, latitude, longitude, visible) values
-  (3, 'Ardennes', 'Forest', 'Belgium', 'Wallonia', 50.25, 5.67, 0);
+INSERT INTO destination (destination_id, profile_id, name, type, country, district, latitude, longitude, visible) values
+  (4, 3, 'Ardennes', 'Forest', 'Belgium', 'Wallonia', 50.25, 5.67, 0);
 
-INSERT INTO trip(name, profile_id) VALUES ('Johnny Trip', 3);
+INSERT INTO trip(trip_id, name, profile_id) VALUES (1, 'Johnny Trip', 3);
 
 INSERT INTO trip_destination(trip_id, destination_id, arrival, departure, dest_order) values
   (1, 3, CURRENT_DATE, CURRENT_DATE, 1);
