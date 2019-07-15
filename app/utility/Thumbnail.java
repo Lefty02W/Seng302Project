679f562130264ca0812f9f72aa0b435fc3caed83
Harry Feasey <hef25@uclive.ac.nz>
2019-07-15 14:57:56 +1200

    Template method and javadoc it.  #document #story[426]

--- app/utility/Thumbnail.java
+++ app/utility/Thumbnail.java
@@ -1,5 +1,9 @@
 package utility;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
 public class Thumbnail {
 
     private static Thumbnail INSTANCE;
@@ -16,4 +20,21 @@ public class Thumbnail {
     }
 
     private Thumbnail(){}
+
+    /**
+     * This method will take in a buffered image at full size and return
+     * a 100x100 pixel thumbnail version of it.
+     * @param sourceImage The BufferedImage object to be converted to thumbnail
+     * @return The thumbnail as a BufferedImage object
+     */
+    public BufferedImage extract(BufferedImage sourceImage) {
+
+        // check x and y lengths and throw IllegalArgumentException
+        // resize buffered image using:
+            //https://www.techcoil.com/blog/how-to-create-a-thumbnail-of-an-image-in-java-without-using-external-libraries/
+       return null;
+
+    }
+
+
 }
