88ededbc70bb7431a6c41bc4d27f3237b765f957
hef25 <hef25@uclive.ac.nz>
2019-07-18 16:50:16 +1200

    Setup boilerplate TreasureHuntRepository #story[428] #implement

---
+++ app/repository/TreasureHuntRepository.java
@@ -0,0 +1,25 @@
+package repository;
+
+import io.ebean.Ebean;
+import io.ebean.EbeanServer;
+import play.db.ebean.EbeanConfig;
+
+import javax.inject.Inject;
+
+public class TreasureHuntRepository {
+
+
+    private final EbeanServer ebeanServer;
+    private final DatabaseExecutionContext executionContext;
+
+    @Inject
+    public TreasureHuntRepository(EbeanConfig ebeanConfig, DatabaseExecutionContext executionContext) {
+        this.ebeanServer = Ebean.getServer(ebeanConfig.defaultServer());
+        this.executionContext = executionContext;
+    }
+
+
+
+
+
+}
