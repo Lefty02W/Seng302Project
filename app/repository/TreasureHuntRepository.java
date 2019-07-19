f37dedbbda923439c513772c8ac8c79ed92b1e31
hef25 <hef25@uclive.ac.nz>
2019-07-19 14:07:13 +1200

    Create TreasureHuntRepository insert method #story[428] #implement

--- app/repository/TreasureHuntRepository.java
+++ app/repository/TreasureHuntRepository.java
@@ -2,9 +2,13 @@ package repository;
 
 import io.ebean.Ebean;
 import io.ebean.EbeanServer;
+import models.TreasureHunt;
 import play.db.ebean.EbeanConfig;
 
 import javax.inject.Inject;
+import java.util.concurrent.CompletionStage;
+
+import static java.util.concurrent.CompletableFuture.supplyAsync;
 
 public class TreasureHuntRepository {
 
@@ -18,7 +22,18 @@ public class TreasureHuntRepository {
         this.executionContext = executionContext;
     }
 
-
+    /**
+     * Inserts an TreasureHunt object into the ebean database server
+     *
+     * @param treasureHunt TreasureHunt object to insert into the database
+     * @return the TreasureHunt id
+     */
+    public CompletionStage<Integer> insert(TreasureHunt treasureHunt){
+        return supplyAsync(() -> {
+            ebeanServer.insert(treasureHunt);
+            return treasureHunt.getTreasureHuntId();
+        }, executionContext);
+    }
 
 
 
