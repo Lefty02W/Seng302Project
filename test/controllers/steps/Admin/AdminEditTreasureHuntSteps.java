3133dd607c7109d09faa091c6ca68ba69756823a
Chuan Law <cll62@uclive.ac.nz>
2019-07-21 20:26:48 +1200

    Wrote some valid edit treasure hunt cucumber tests
    - Created a new feature file to define admin edit treasure hunt
    scenarios
    - Defined new steps for the feature file
    
    #story[428]

--- test/controllers/steps/Admin/AdminEditTreasureHuntSteps.java
+++ test/controllers/steps/Admin/AdminEditTreasureHuntSteps.java
@@ -7,6 +7,8 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import models.Destination;
+import models.TreasureHunt;
+import org.junit.Assert;
 import play.mvc.Http;
 import play.mvc.Result;
 import play.test.Helpers;
@@ -19,19 +21,37 @@ import static org.junit.Assert.assertEquals;
 
 public class AdminEditTreasureHuntSteps  extends ProvideApplication {
 
-    Map<String, String> loginForm = new HashMap<>();
     Map<String, String> huntForm = new HashMap<>();
-    Result loginResult;
+    private Result redirectDestination;
 
+    @When("^admin presses edit on one of the treasure hunts$")
+    public void adminPressesEditOnOneOfTheTreasureHunts() throws Throwable {
+        Http.RequestBuilder request = Helpers.fakeRequest()
+                .method("GET")
+                .uri("/admin/hunts/2/edit/show")
+                .session("connected", "1");
+        Result result = Helpers.route(provideApplication(), request);
+        injectRepositories();
+        TreasureHunt hunt = treasureHuntRepository.lookup(2);
+        huntForm.put("riddle", hunt.getRiddle());
+        huntForm.put("destinationId", Integer.toString(hunt.getTreasureHuntDestinationId()));
+        huntForm.put("endDate", hunt.getEndDateString());
+        huntForm.put("startDate", hunt.getStartDateString());
+    }
 
-    @When("^admin selects edit on treasure hunt (\\d+)$")
+    @And("^admin selects edit on treasure hunt (\\d+)$")
     public void adminSelectsEditOnTreasureHunt(int arg0) throws Throwable {
-        huntForm.put("treasureHuntId", Integer.toString(arg0));
+        huntForm.put("destinationId", Integer.toString(arg0));
     }
 
     @And("^changes the riddle to \"([^\"]*)\"$")
     public void changesTheRiddleTo(String arg0) throws Throwable {
-        huntForm.put("riddle", "A concrete jungle");
+        huntForm.put("riddle", arg0);
+    }
+
+    @And("^changes the start date to \"([^\"]*)\"$")
+    public void changesTheStartDateTo(String arg0) throws Throwable {
+        huntForm.put("startDate", arg0);
     }
 
     @And("^selects the the save treasure hunt button$")
@@ -39,9 +59,31 @@ public class AdminEditTreasureHuntSteps  extends ProvideApplication {
         Http.RequestBuilder request = Helpers.fakeRequest()
                 .method("POST")
                 .uri("/admin/hunts/2/edit")
+                .bodyForm(huntForm)
                 .session("connected", "2");
-        Result result = Helpers.route(provideApplication(), request);
+        redirectDestination = Helpers.route(provideApplication(), request);
+    }
 
-        assertEquals(200, result.status());
+    @Then("^I am redirected to the admin page$")
+    public void iAmRedirectedToTheTreasureHuntsPage() throws Throwable {
+        if (redirectDestination.redirectLocation().isPresent()) {
+            assertEquals("/admin", redirectDestination.redirectLocation().get());
+        } else {
+            Assert.fail();
+        }
+    }
+
+    @Then("^The admins riddle is updated in the database$")
+    public void theAdminsRiddleIsUpdatedInTheDatabase() throws Throwable {
+        injectRepositories();
+        TreasureHunt hunt = treasureHuntRepository.lookup(2);
+        assertEquals("A concrete jungle", hunt.getRiddle());
+    }
+
+    @Then("^The admins start date is updated in the database$")
+    public void theAdminsStartDateIsUpdatedInTheDatabase() throws Throwable {
+        injectRepositories();
+        TreasureHunt hunt = treasureHuntRepository.lookup(2);
+        assertEquals("2019-01-04", hunt.getStartDateString());
     }
 }
--- test/features/Admin/AdminEditTreasureHunt.feature
+++ test/features/Admin/AdminEditTreasureHunt.feature
@@ -3,11 +3,22 @@ Feature: Edit a treasure hunt from admin page
   I want to be able to edit all users treasure hunts
   So I can keep all treasure hunts consistent and up to date
 
-#  Scenario: Admin edits a valid treasure hunt
-#    Given Admin is logged in to the application
-#    And admin is on the admin page
-#    When admin selects edit on treasure hunt 2
-#    And I set the riddle to "A new riddle"
-#    And selects the the save treasure hunt button
-#    Then I am redirected to the treasure hunts page
-#    Then The riddle is updated in the database
+  Scenario: Admin edits a valid treasure hunt with riddle
+    Given Admin is logged in to the application
+    And admin is on the admin page
+    When admin presses edit on one of the treasure hunts
+    And admin selects edit on treasure hunt 2
+    And changes the riddle to "A concrete jungle"
+    And selects the the save treasure hunt button
+    Then I am redirected to the admin page
+    Then The admins riddle is updated in the database
+
+  Scenario: Admin edits a valid treasure hunt with start time
+    Given Admin is logged in to the application
+    And admin is on the admin page
+    When admin presses edit on one of the treasure hunts
+    And admin selects edit on treasure hunt 2
+    And changes the start date to "2019-01-04"
+    And selects the the save treasure hunt button
+    Then I am redirected to the admin page
+    Then The admins start date is updated in the database
