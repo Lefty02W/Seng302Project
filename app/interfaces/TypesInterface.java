16853b76ef9b7d14f7c45297fdfbed0d03a54286
George K <gkh36@uclive.ac.nz>
2019-07-27 11:57:20 +1200

    Make the undo() interface method asynchronous
    - Added some comments for further explanation
    
    #implement #story[430]

--- app/interfaces/TypesInterface.java
+++ app/interfaces/TypesInterface.java
@@ -1,5 +1,19 @@
 package interfaces;
 
+import java.util.concurrent.CompletionStage;
+
+/**
+ * Purpose of the interface is to allow different
+ * object types to implement their own undo method
+ * Examples of 'types' include Profile, Destinations etc.
+ */
 public interface TypesInterface {
-    void undo(int objectID);
+    
+    /**
+     * Method to perform an undo action.
+     * The method is expected to operate asynchronously
+     * @param objectID - ID of the object to have the undo operation performed against
+     * @return null
+     */
+    CompletionStage<Void> undo(int objectID);
 }
