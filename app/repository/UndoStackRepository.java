f3ad0da84a8b555ba5472b9b98141f08c6eb2d5f
Luke Walsh <lwa383@uclive.ac.nz>
2019-07-26 17:54:27 +1200

    Implemented repository methods to add and remove objects from the db
    - Implemented method to insert a soft deleted item into the undo stack
    - Implemented method to get a list of an admins stack
    - Implemented method to clear an admins stack
    - Implemented method to remove a given object from the undoStack
    
    #story[430]

--- app/repository/UndoStackRepository.java
+++ app/repository/UndoStackRepository.java
@@ -2,9 +2,14 @@ package repository;
 
 import io.ebean.Ebean;
 import io.ebean.EbeanServer;
+import models.UndoStack;
 import play.db.ebean.EbeanConfig;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.concurrent.CompletionStage;
+
+import static java.util.concurrent.CompletableFuture.supplyAsync;
 
 public class UndoStackRepository {
 
@@ -16,5 +21,63 @@ public class UndoStackRepository {
         this.ebeanServer = Ebean.getServer(ebeanConfig.defaultServer());
         this.executionContext = executionContext;
     }
+
+    /**
+     * Method to insert a soft deleted item into the undo stack
+     * @param undoItem object holding the variables required for inserting item into the undoStack database table
+     * @return null
+     */
+    public CompletionStage<Void> addToStack(UndoStack undoItem){
+        return supplyAsync(() -> {
+            ebeanServer.insert(undoItem);
+            return null;
+        }, executionContext);
+    }
+
+    /**
+     * Method to check if the current user has any changes on the stack
+     * @param userId id of the current user
+     * @return list of current users stack
+     */
+    public ArrayList<UndoStack> getUsersStack(int userId){
+        return new ArrayList<>(ebeanServer.find(UndoStack.class)
+                .where()
+                .eq("profile_id", userId)
+                .findList());
+    }
+
+    /**
+     * Method to remove all items from undoStack for a given user
+     * @param userId id of the user who's stack will be cleared
+     * @return null
+     */
+    public CompletionStage<Void> clearStack(int userId){
+        // TODO: 26/07/19 test that this deletes all items and not just the first 
+        return supplyAsync(() -> {
+            ebeanServer.find(UndoStack.class)
+                    .where()
+                    .eq("profile_id", userId)
+                    .delete();
+            return null;
+        });
+    }
+
+    /**
+     * Deletes an item from the database
+     * @param item the item object to be removed from the database
+     * @return null
+     */
+    public CompletionStage<Void> removeItem(UndoStack item){
+        return supplyAsync(() -> {
+            ebeanServer.find(UndoStack.class)
+                    .where()
+                    .eq("profile_id", item.getProfileId())
+                    .eq("object_id", item.getObjectId())
+                    .delete();
+            return null;
+        });
+    }
+
+
 
 }
