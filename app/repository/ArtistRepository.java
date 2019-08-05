e7881a9acc32bf79305828abbab2c0e8951ecf35
Harry <hef25@uclive.ac.nz>
2019-08-05 15:41:15 +1200

    Add method to get all of users artists. Clear up unused imports #story[493] #implement

--- app/repository/ArtistRepository.java
+++ app/repository/ArtistRepository.java
@@ -3,9 +3,14 @@ package repository;
 import io.ebean.Ebean;
 import io.ebean.EbeanServer;
 import models.Artist;
+import models.ArtistProfile;
+import models.Destination;
+import models.TreasureHunt;
 import play.db.ebean.EbeanConfig;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletionStage;
 
 import static java.util.concurrent.CompletableFuture.supplyAsync;
@@ -36,13 +41,43 @@ public class ArtistRepository {
      * Inserts an Artist object into the ebean database server
      *
      * @param artist Artist object to insert into the database
-     * @return the Artist id
+     * @return the new Artist id
      */
-    public CompletionStage<Integer> insert(Artist artist) {
+    public CompletionStage<Integer> insert(Artist artist, List<ArtistProfile> artistProfiles) {
         return supplyAsync(() -> {
+
             ebeanServer.insert(artist);
+
+            //Manually applying linkage to profiles.
+            for(ArtistProfile artistProfile : artistProfiles) {
+                ebeanServer.insert(artistProfile);
+            }
             return artist.getArtistId();
         }, executionContext);
+    }
+
+    /**
+     * Method to return all of a users artists
+     * @param userId, the id of the user
+     * @return Artists, an ArrayList of all artists that user is a part of.
+     */
+    public List<Artist> getAllUserArtists(int userId) {
+
+        List<ArtistProfile> artistProfiles = new ArrayList<>(ebeanServer.find(ArtistProfile.class)
+                .where()
+                .eq("soft_delete", 0)
+                .eq("profile_id", userId)
+                .findList());
+
+        List<Artist> artists = new ArrayList<>();
+        for(ArtistProfile artistProfile : artistProfiles) {
+            artists.add(ebeanServer.find(Artist.class)
+                    .where()
+                    .eq("soft_delete", 0)
+                    .eq("artist_id", artistProfile.getAPArtistId())
+                    .findOne());
+        }
+        return artists;
     }
 
 
