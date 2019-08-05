ecb57acf84d9a448f8d1dafd00178a65fdb1a4e5
Luke Walsh <lwa383@uclive.ac.nz>
2019-08-05 13:08:57 +1200

    Created and implemented Artist profile model
    - Created new java class to hold the ArtistProfile object so we can
    easily use ebean database operations
    - Added all properties we expect the profile to require following the AC's
    
    #story[493]

---
+++ app/models/ArtistProfile.java
@@ -0,0 +1,138 @@
+package models;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class ArtistProfile {
+
+    private Integer artistId;
+
+    private String artistName;
+
+    private String biography;
+
+    private String facebookLink;
+
+    private String instagramLink;
+
+    private String spotifyLink;
+
+    private String twitterLink;
+
+    private String websiteLink;
+
+    private Collection<Profile> membersList;
+
+    private Map<Integer, PassportCountry> country;
+
+    private int softDelete;
+
+    /**
+     * Traditional constructor used for retrieving object from DB
+     * @param artistId
+     * @param artistName
+     * @param biography
+     * @param facebookLink
+     * @param instagramLink
+     * @param spotifyLink
+     * @param twitterLink
+     * @param websiteLink
+     * @param membersList
+     * @param country
+     */
+    public ArtistProfile(Integer artistId, String artistName, String biography, String facebookLink, String instagramLink, String spotifyLink, String twitterLink, String websiteLink, Collection<Profile> membersList, Map<Integer, PassportCountry> country, int softDelete) {
+        this.artistId = artistId;
+        this.artistName = artistName;
+        this.biography = biography;
+        this.facebookLink = facebookLink;
+        this.instagramLink = instagramLink;
+        this.spotifyLink = spotifyLink;
+        this.twitterLink = twitterLink;
+        this.websiteLink = websiteLink;
+        this.membersList = membersList;
+        this.country = country;
+        this.softDelete = softDelete;
+    }
+
+    //Getters and setters
+
+    public Integer getArtistId() {
+        return artistId;
+    }
+
+    public void setArtistId(Integer artistId) {
+        this.artistId = artistId;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public String getFacebookLink() {
+        return facebookLink;
+    }
+
+    public void setFacebookLink(String facebookLink) {
+        this.facebookLink = facebookLink;
+    }
+
+    public String getInstagramLink() {
+        return instagramLink;
+    }
+
+    public void setInstagramLink(String instagramLink) {
+        this.instagramLink = instagramLink;
+    }
+
+    public String getSpotifyLink() {
+        return spotifyLink;
+    }
+
+    public void setSpotifyLink(String spotifyLink) {
+        this.spotifyLink = spotifyLink;
+    }
+
+    public String getTwitterLink() {
+        return twitterLink;
+    }
+
+    public void setTwitterLink(String twitterLink) {
+        this.twitterLink = twitterLink;
+    }
+
+    public String getWebsiteLink() {
+        return websiteLink;
+    }
+
+    public void setWebsiteLink(String websiteLink) {
+        this.websiteLink = websiteLink;
+    }
+
+    public Collection<Profile> getMembersList() {
+        return membersList;
+    }
+
+    public void setMembersList(Collection<Profile> membersList) {
+        this.membersList = membersList;
+    }
+
+    public Map<Integer, PassportCountry> getCountry() {
+        return country;
+    }
+
+    public void setCountry(Map<Integer, PassportCountry> country) {
+        this.country = country;
+    }
+}
