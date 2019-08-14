64045f908be998fe85816c1b9288b3c5ffecea8d
Jade <jma361@uclive.ac.nz>
2019-08-15 00:02:21 +1200

    Implemented new tables needed for all events and linking tables     - added all foreign key constrants         - made it cascade when needed on updates and deletes  - Added sql scripts to test database
    #story[496]

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -413,6 +413,73 @@ create table artist_country
 ;
 
 
+create table if not exists events
+(
+	event_id int auto_increment,
+	event_name varchar(255) not null,
+	description varchar(255) null,
+	destination_id int not null,
+	start_date date null,
+	end_date date null,
+	age_restriction int null,
+	constraint events_event_id_uindex
+		unique (event_id),
+	constraint destination_id_____destination_fk
+		foreign key (destination_id) references destination (destination_id)
+			on update cascade on delete cascade
+);
+
+alter table events
+	add primary key (event_id);
+
+create table if not exists type_of_events
+(
+	type_id int auto_increment,
+	type_name varchar(50) not null,
+	constraint type_of_events_type_id_uindex
+		unique (type_id)
+);
+
+alter table type_of_events
+	add primary key (type_id);
+
+create table if not exists event_type
+(
+	event_id int not null,
+	type_id int not null,
+	constraint event_id_____events_fk
+		foreign key (event_id) references events (event_id)
+			on update cascade on delete cascade,
+	constraint type_id_____type_of_events_fk
+		foreign key (type_id) references type_of_events (type_id)
+			on update cascade on delete cascade
+);
+
+create table if not exists event_genres
+(
+	event_id int not null,
+	genre_id int not null,
+	constraint event_id_genres_____events_fk
+		foreign key (event_id) references events (event_id)
+			on update cascade on delete cascade,
+	constraint genre_id_____music_genre_fk
+		foreign key (genre_id) references music_genre (genre_Id)
+			on update cascade on delete cascade
+);
+
+create table if not exists artist_events
+(
+	artist_id int not null,
+	event_id int not null,
+	constraint artist_id_events_____artist_fk
+		foreign key (artist_id) references artist (artist_id)
+			on update cascade on delete cascade,
+	constraint event_id_events_____events_fk
+		foreign key (event_id) references events (event_id)
+			on update cascade on delete cascade
+);
+
+
 
 
 
@@ -475,3 +542,14 @@ drop table if exists music_genre;
 drop table if exists artist_profile;
 
 drop table if exists artist_country;
+
+drop table if exists event;
+
+drop table if exists type_of_events;
+
+drop table if exists artist_events;
+
+drop table if exists event_genres;
+
+drop table if exists event_type;
+
