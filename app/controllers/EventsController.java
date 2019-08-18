02db950f1e2ef44adbff8cd6d0064811990cba3c
Luke Walsh <lwa383@uclive.ac.nz>
2019-08-18 15:56:39 +1200

    Implemented search bar frontend
    - Implemented accordion to hold search and filter functionality
    - Implemented accordion dropdown to hold artist name, event type,
    countries, age restriction and genres search boxes
    -Need to implement date picker to search on dates
    - Updated controller show method to pass in countries and genre lists
    
    #story[496]

--- app/controllers/EventsController.java
+++ app/controllers/EventsController.java
@@ -5,7 +5,9 @@ import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.Result;
 import play.mvc.Security;
+import repository.GenreRepository;
 import repository.ProfileRepository;
+import utility.Country;
 import views.html.events;
 
 import javax.inject.Inject;
@@ -15,11 +17,13 @@ public class EventsController extends Controller {
 
     private final ProfileRepository profileRepository;
     private MessagesApi messagesApi;
+    private final GenreRepository genreRepository;
 
     @Inject
-    public EventsController(ProfileRepository profileRepository, MessagesApi messagesApi){
+    public EventsController(ProfileRepository profileRepository, MessagesApi messagesApi, GenreRepository genreRepository){
         this.profileRepository = profileRepository;
         this.messagesApi = messagesApi;
+        this.genreRepository = genreRepository;
     }
 
     /**
@@ -32,7 +36,9 @@ public class EventsController extends Controller {
         Integer profId = SessionController.getCurrentUserId(request);
 
         return profileRepository.findById(profId)
-                .thenApplyAsync(profileRec -> profileRec.map(profile -> ok(events.render(profile, request, messagesApi.preferred(request))))
+                .thenApplyAsync(profileRec -> profileRec.map(profile -> ok(events.render(profile,
+                        Country.getInstance().getAllCountries(), genreRepository.getAllGenres(),
+                        request, messagesApi.preferred(request))))
                 .orElseGet(() -> redirect("/")));
     }
 }
