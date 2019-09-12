b84d2f4ba89540d0add78ffe972c9fcd1253c8e1
Sam Verdellen <sgv15@uclive.ac.nz>
2019-09-12 15:56:47 +1200

    Working on the backend to allow users to filter to only show their admined artists. Added a database function to get all of a users admined artists and also implemented a way to send this through to the web page. Next job will be to provide the option in the search filter. This took longer as I got stuck on my previous task and I had to spend some time researching how to do this using ebeans. #implement #story[569]

--- app/controllers/ArtistController.java
+++ app/controllers/ArtistController.java
@@ -65,7 +65,7 @@ public class ArtistController extends Controller {
     public CompletionStage<Result> show(Http.Request request) {
         Integer profId = SessionController.getCurrentUserId(request);
         return profileRepository.findById(profId)
-                .thenApplyAsync(profileRec -> profileRec.map(profile -> ok(artists.render(searchForm, profile, genreRepository.getAllGenres(), profileRepository.getAllEbeans(), Country.getInstance().getAllCountries(),  artistRepository.getPagedArtists(0), artistRepository.getFollowedArtists(profId), request, messagesApi.preferred(request)))).orElseGet(() -> redirect("/profile")));
+                .thenApplyAsync(profileRec -> profileRec.map(profile -> ok(artists.render(searchForm, profile, genreRepository.getAllGenres(), profileRepository.getAllEbeans(), Country.getInstance().getAllCountries(),  artistRepository.getPagedArtists(0), artistRepository.getFollowedArtists(profId), profileRepository.getProfileArtists(profId), messagesApi.preferred(request)))).orElseGet(() -> redirect("/profile")));
 
     }
 
@@ -107,7 +107,7 @@ public class ArtistController extends Controller {
 
 
                         searchForm.fill(formData); //TODO Make form fill with previous search
-                        return ok(artists.render(searchForm, profile.get(), genreRepository.getAllGenres(), profileRepository.getAllEbeans(), Country.getInstance().getAllCountries(), artistRepository.searchArtist(formData.name, formData.genre, formData.country, followed, profId), artistRepository.getFollowedArtists(profId), request, messagesApi.preferred(request)));
+                        return ok(artists.render(searchForm, profile.get(), genreRepository.getAllGenres(), profileRepository.getAllEbeans(), Country.getInstance().getAllCountries(), artistRepository.searchArtist(formData.name, formData.genre, formData.country, followed, profId), artistRepository.getFollowedArtists(profId), profileRepository.getProfileArtists(profId), request, messagesApi.preferred(request)));
                     } else {
                         return redirect("/artists");
                     }
