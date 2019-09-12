8c9a0d87bbc8da6779d3b7a8679fba05e1c5704c
lgg24 <lgg24@uclive.ac.nz>
2019-09-12 16:39:27 +1200

    Basic implementation for inserting and event type #implement #story[568]

--- scripts/events.py
+++ scripts/events.py
@@ -1,19 +1,36 @@
 from readJSON import *
+import random
 
 
-def event_name_exists(event_name, cursor, db):
+def get_event_id(event_name, cursor, db):
     """Search in the database to see if an event with that name already exists, return true if yes, false if not"""
     try:
-        cursor.execute("SELECT event_name from events WHERE event_name = '{}'".format(event_name))
+        cursor.execute("SELECT event_id from events WHERE event_name = '{}'".format(event_name))
         db.commit()
-        name = cursor.fetchone()[0]
-        if name is not None:
-            return True
+        id = cursor.fetchone()[0]
+        if id is not None:
+            return id
         else:
-            return False
+            return 0
     except Exception as e:
         print("Error checking if event with name exists")
         return True
+
+
+def execute_event_type_queries(event_id, cursor, db):
+    """Executes event type queries to insert the event types into the database"""
+    try:
+        cursor.execute("SELECT event_id from event_type WHERE event_id = '{}'".format(event_id))
+        db.commit()
+        id = cursor.fetchone()[0]
+        if id is not None:
+            random.randint(1,4)
+            cursor.execute("INSERT INTO event_type (event_id, type_id) VALUES ('{}', '{}')".format(event_id, random.randint(1, 4)))
+            db.commit()
+    except Exception as e:
+        db.rollback()
+        print("\nError inserting event types")
+
 
 
 def execute_event_queries(cursor, db, number_events, number_artists, number_destinations):
@@ -32,10 +49,11 @@ def execute_event_queries(cursor, db, number_events, number_artists, number_dest
     artists = read_artists() # todo make this work
     for event in events_list:
         try:
-            if event_name_exists(event[0], cursor, db):
+            if get_event_id(event[0], cursor, db) == 0:
                 cursor.execute("INSERT INTO events (event_name, description, destination_id, start_date, end_date, age_restriction) VALUES ('{}', '{}')".format(event[0], event[1], event[2], event[3], event[4]))
                 db.commit()
-                #insert event type
+                event_id = get_event_id(event[0], cursor, db)
+                execute_event_type_queries(event_id, cursor, db)
                 #insert event genres
                 #insert event artists
         except Exception as e:
