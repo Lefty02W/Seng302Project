bbe8e3c7f749ed39dc23140ff2f85bca8431c709
George K <gkh36@uclive.ac.nz>
2019-09-18 12:20:14 +1200

    Writing tests for the search scenarios, some TBD, added a route for search
    #test #story[627]

--- test/controllers/steps/Destinations/SearchDestinationsSteps.java
+++ test/controllers/steps/Destinations/SearchDestinationsSteps.java
@@ -12,19 +12,33 @@ import play.test.Helpers;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.junit.Assert.assertEquals;
+
 public class SearchDestinationsSteps {
     private Map<String, String> loginForm = new HashMap<>();
     private Map<String, String> destForm = new HashMap<>();
+    private Map<String, String> searchForm = new HashMap<>();
     private Result redirectDestination;
+
 
     @When("user searches for a public destination with name {string}")
     public void userSearchesForAPublicDestinationWithName(String name) {
-        throw new cucumber.api.PendingException();
+        searchForm.put("isPublic", "true");
+        searchForm.put("name", name);
     }
 
     @When("uer searches for a private destination with name {string}")
     public void uerSearchesForAPrivateDestinationWithName(String name) {
-        throw new cucumber.api.PendingException();
+        searchForm.put("isPublic", "false");
+        searchForm.put("name", name);
+
+        Http.RequestBuilder request = Helpers.fakeRequest()
+                .method("POST")
+                .uri("/destinations/search/0")
+                .bodyForm(searchForm)
+                .session("connected", "1");
+        redirectDestination = Helpers.route(TestApplication.getApplication(), request);
+        assertEquals(303, redirectDestination.status());
     }
 
     @Then("the destination {string} is displayed in the search result")
@@ -35,24 +49,29 @@ public class SearchDestinationsSteps {
     @Then("the destination {string} is not displayed in the search result")
     public void theDestinationIsNotDisplayedInTheSearchResult(String arg0) {
         throw new cucumber.api.PendingException();
-
     }
 
     @And("the search result is empty")
     public void theSearchResultIsEmpty() {
         throw new cucumber.api.PendingException();
-
     }
 
     @When("user submits empty search")
     public void userSubmitsEmptySearch() {
-        throw new cucumber.api.PendingException();
+        searchForm.put("name", "");
+        Http.RequestBuilder request = Helpers.fakeRequest()
+                .method("POST")
+                .uri("/destinations/search/0")
+                .bodyForm(searchForm)
+                .session("connected", "1");
+        redirectDestination = Helpers.route(TestApplication.getApplication(), request);
+        assertEquals(303, redirectDestination.status());
     }
 
 
     @Then("an error message should be shown telling the user to enter a name")
     public void anErrorMessageShouldBeShownTellingTheUserToEnterAName() {
-        throw new cucumber.api.PendingException();
+        Assert.assertTrue(redirectDestination.flash().getOptional("error").isPresent());
     }
 
 
