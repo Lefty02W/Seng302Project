38bf65f1af7ee9dc1472e77b26c99dbd7f16c22f
George K <gkh36@uclive.ac.nz>
2019-09-20 15:03:53 +1200

    Re-enabled and fixed tests for destination search
    #test #fix #story[627]

--- test/controllers/steps/Destinations/SearchDestinationsSteps.java
+++ test/controllers/steps/Destinations/SearchDestinationsSteps.java
@@ -5,33 +5,32 @@ import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import models.Destination;
 import org.junit.Assert;
+import org.junit.Test;
 import play.mvc.Http;
 import play.mvc.Result;
 import play.test.Helpers;
+import repository.DestinationRepository;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class SearchDestinationsSteps {
     private Map<String, String> loginForm = new HashMap<>();
     private Map<String, String> destForm = new HashMap<>();
     private Map<String, String> searchForm = new HashMap<>();
     private Result redirectDestination;
-
-
-    @And("the search result is empty")
-    public void theSearchResultIsEmpty() {
-        throw new cucumber.api.PendingException();
-    }
+    private DestinationRepository destinationRepository = TestApplication.getDestinationRepository();
 
     @When("user submits empty search")
     public void userSubmitsEmptySearch() {
         searchForm.put("name", "");
         Http.RequestBuilder request = Helpers.fakeRequest()
-                .method("POST")
+                .method("GET")
                 .uri("/destinations/search/0")
                 .bodyForm(searchForm)
                 .session("connected", "1");
@@ -52,19 +51,25 @@ public class SearchDestinationsSteps {
         searchForm.put("name", name);
 
         Http.RequestBuilder request = Helpers.fakeRequest()
-                .method("POST")
+                .method("GET")
                 .uri("/destinations/search/0")
                 .bodyForm(searchForm)
                 .session("connected", "1");
         redirectDestination = Helpers.route(TestApplication.getApplication(), request);
 
-        assertEquals(303, redirectDestination.status());
+        assertEquals(200, redirectDestination.status());
     }
 
-    @Then("^the destination \"([^\"]*)\" is displayed in the search result$")
-    public void theDestinationIsDisplayedInTheSearchResult(String arg0) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @Then("^the private destination \"([^\"]*)\" is displayed in the search result$")
+    public void thePrivateDestinationIsDisplayedInTheSearchResult(String name) throws Throwable {
+        List<Destination> destinationList = destinationRepository.searchDestinations(name, 0, false, 1);
+        assertEquals(true, destinationInList(name, destinationList));
+    }
+
+    @Then("^the public destination \"([^\"]*)\" is displayed in the search result$")
+    public void thePublicDestinationIsDisplayedInTheSearchResult(String name) throws Throwable {
+        List<Destination> destinationList = destinationRepository.searchDestinations(name, 0, true, 1);
+        assertEquals(true, destinationInList(name, destinationList));
     }
 
     @When("^user searches for a public destination with name \"([^\"]*)\"$")
@@ -73,18 +78,40 @@ public class SearchDestinationsSteps {
         searchForm.put("name", name);
 
         Http.RequestBuilder request = Helpers.fakeRequest()
-                .method("POST")
+                .method("GET")
                 .uri("/destinations/search/0")
                 .bodyForm(searchForm)
                 .session("connected", "1");
         redirectDestination = Helpers.route(TestApplication.getApplication(), request);
 
-        assertEquals(303, redirectDestination.status());
+        assertEquals(200, redirectDestination.status());
     }
 
-    @Then("^the destination \"([^\"]*)\" is not displayed in the search result$")
-    public void theDestinationIsNotDisplayedInTheSearchResult(String arg0) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+
+    /**
+     * Check if a destination list contains a destination of matching name
+     * @param name - Name of destination
+     * @param destinationList - List of destinations found from search
+     * @return boolean - True if destination in list, false otherwise
+     */
+    private boolean destinationInList(String name, List<Destination> destinationList) {
+        for (Destination destination: destinationList) {
+            if (destination.getName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Then("^the private destination \"([^\"]*)\" is not displayed in the search result$")
+    public void thePrivateDestinationIsNotDisplayedInTheSearchResult(String name) throws Throwable {
+        List<Destination> destinationList = destinationRepository.searchDestinations(name, 0, false, 1);
+        assertEquals(false, destinationInList(name, destinationList));
+    }
+
+    @Then("^the public destination \"([^\"]*)\" is not displayed in the search result$")
+    public void thePublicDestinationIsNotDisplayedInTheSearchResult(String name) {
+        List<Destination> destinationList = destinationRepository.searchDestinations(name, 0, true, 1);
+        assertEquals(false, destinationInList(name, destinationList));
     }
 }
--- test/features/Destinations/SearchDestinations.feature
+++ test/features/Destinations/SearchDestinations.feature
@@ -6,33 +6,31 @@ Feature: Search for a destination
   Scenario: Search for a public destination
     Given User is logged in to the application
     And user is at the destinations page
-    #When user searches for a public destination with name "Christchurch"
-    Then the destination "Christchurch" is displayed in the search result
+    When user searches for a public destination with name "New York"
+    Then the public destination "New York" is displayed in the search result
 
   Scenario: Search for a private destination
     Given User is logged in to the application
     And user is at the destinations page
-    #When user searches for a private destination with name "Home"
-    Then the destination "Home" is displayed in the search result
+    When user searches for a private destination with name "Tokyo"
+    Then the private destination "Tokyo" is displayed in the search result
 
   Scenario: Search for a non-existent public destination
     Given User is logged in to the application
     And user is at the destinations page
-    #When user searches for a public destination with name "Thisisalongstringtotestthedestinationssearchfunctionality"
-    Then the destination "Thisisalongstringtotestthedestinationssearchfunctionality" is not displayed in the search result
-    And the search result is empty
+    When user searches for a public destination with name "Thisisalongstringtotestthedestinationssearchfunctionality"
+    Then the private destination "Thisisalongstringtotestthedestinationssearchfunctionality" is not displayed in the search result
 
   Scenario: Search for a non-existent private destination
     Given User is logged in to the application
     And user is at the destinations page
-    #When user searches for a private destination with name "Thisisanotherlongstringtotestthedestinationssearchfunctionality"
-    Then the destination "Thisisanotherlongstringtotestthedestinationssearchfunctionality" is not displayed in the search result
-    And the search result is empty
+    When user searches for a private destination with name "Thisisanotherlongstringtotestthedestinationssearchfunctionality"
+    Then the public destination "Thisisanotherlongstringtotestthedestinationssearchfunctionality" is not displayed in the search result
 
   Scenario: Empty search
     Given User is logged in to the application
     And user is at the destinations page
-    #When user submits empty search
-    #Then an error message should be shown telling the user to enter a name
+    When user submits empty search
+    Then an error message should be shown telling the user to enter a name
 
 
