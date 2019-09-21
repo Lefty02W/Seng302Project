acf3d7dbf4b50ef79dcf975f6aa1ddc5ba9855e0
Sam Verdellen <sgv15@uclive.ac.nz>
2019-09-21 14:30:46 +1200

    Events search is now paginated. Fixed the problem I ran into yesterday. Updated all of the paginated pages to grey out the buttons which are disabled. Did this by adjusting the css and adding a class to the disabled buttons. #implement #pair[sgv15, jma361] #story[627]

--- app/controllers/EventsController.java
+++ app/controllers/EventsController.java
@@ -361,6 +361,7 @@ public class EventsController extends Controller {
             if(profile.isPresent()){
                 Form<EventFormData> searchEventForm = eventFormDataForm.bindFromRequest(request);
                 EventFormData eventFormData = searchEventForm.get();
+                System.out.println("Search Here: " + searchEventForm);
                 if(eventFormData.getAgeRestriction().equals("") && eventFormData.getArtistName().equals("") &&
                 eventFormData.getDestinationId().equals("") && eventFormData.getEventName().equals("") && eventFormData.getEventType().equals("") &&
                 eventFormData.getGenre().equals("") && eventFormData.getStartDate().equals("")) {
@@ -368,7 +369,7 @@ public class EventsController extends Controller {
                 }
 
                 List<Events> eventsList = eventRepository.searchEvent(eventFormData, offset);
-                if(!eventsList.isEmpty()){
+                if(!eventsList.isEmpty() || offset > 0){
                     PaginationHelper paginationHelper = new PaginationHelper(offset, offset, offset, 0, true, true, eventRepository.getNumEvents());
                     paginationHelper.alterNext(8);
                     paginationHelper.alterPrevious(8);
