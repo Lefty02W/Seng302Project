f3fd7fd002a75fdf34b6ab96013bfbb71a5134c4
Ambrose Ledbrook <ajl190@uclive.ac.nz>
2019-09-22 17:57:42 +1200

    Starting to test upcoming events function
    - Running into issue with dates in the test data
    
    #story[630]

--- test/repository/EventRepositoryTest.java
+++ test/repository/EventRepositoryTest.java
@@ -13,17 +13,35 @@ public class EventRepositoryTest {
 
     private final int ARTIST_WITH_EVENTS = 1;
     private final int ARTIST_WITHOUT_EVENTS = 4;
+    private final int PROFILE_WITH_MORE_THAN_TEN = 2;
+    private final int PROFILE_WITH_LESS_THAN_TEN = 13;
+    private final int PROFILE_WITH_EXPIRED = 14;
 
     @Test
     public void getArtistEventsArtistHasEvents() {
         List<Events> events = TestApplication.getEventRepository().getArtistEventsPage(ARTIST_WITH_EVENTS, 0);
-        assertEquals(2, events.size());
+        assertEquals(7, events.size());
     }
 
     @Test
     public void getArtistEventsArtistHasNoEvents() {
         List<Events> events = TestApplication.getEventRepository().getArtistEventsPage(ARTIST_WITHOUT_EVENTS, 0);
         assertTrue(events.isEmpty());
+    }
+
+    @Test
+    public void getNextTenUpComingEventsUserHasMoreThanTen() {
+        //System.out.println(TestApplication.getEventRepository().getNextTenUpComingEvents(PROFILE_WITH_MORE_THAN_TEN));
+    }
+
+    @Test
+    public void getNextTenUpComingEventsUserHasMoreLessTen() {
+
+    }
+
+    @Test
+    public void getNextTenUpComingEventsUserHasExpiredEvents() {
+
     }
 
 }
