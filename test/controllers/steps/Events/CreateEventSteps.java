4f9232a45b9c6c98bb1494f5453bbaee1ec55c36
jma361 <jma361@uclive.ac.nz>
2019-09-22 12:09:23 +1200

    Started writing steps and implementing them for searching events and filtering it by attending or not  - fixed artist route
    #fix #test #story[630]

--- test/controllers/steps/Events/CreateEventSteps.java
+++ test/controllers/steps/Events/CreateEventSteps.java
@@ -49,6 +49,4 @@ public class CreateEventSteps {
 
         assertTrue(eventCreateResult.flash().getOptional(arg0).isPresent());
     }
-
-
 }
---
+++ test/controllers/steps/Events/SearchEventSteps.java
@@ -0,0 +1,61 @@
+package controllers.steps.Events;
+
+import controllers.TestApplication;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import models.EventFormData;
+import models.Events;
+import play.mvc.Http;
+import play.mvc.Result;
+import play.test.Helpers;
+import repository.EventRepository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class SearchEventSteps {
+
+    private Map<String, String> searchForm = new HashMap<>();
+    private Result eventSearchResult;
+    private EventRepository eventRepository = TestApplication.getEventRepository();
+
+    @Given("^I am on the events page$")
+    public void iAmOnTheArtistCreatePage() throws Throwable {
+        Http.RequestBuilder requestDest = Helpers.fakeRequest()
+                .method("GET")
+                .uri("/events")
+                .session("connected", "2");
+        Helpers.route(TestApplication.getApplication(), requestDest);
+    }
+
+    @And("^I select attending from the advance search field$")
+    public void iSelectAttendingFromTheAdvanceSearchField() throws Throwable {
+        searchForm.put("attending", "on");
+    }
+
+    @And("^I submit the search form$")
+    public void iSubmitTheSearchForm() throws Throwable {
+        Http.RequestBuilder request = Helpers.fakeRequest()
+                .method("GET")
+                .uri("/events/all/search/0")
+                .bodyForm(searchForm)
+                .session("connected", "1");
+        eventSearchResult = Helpers.route(TestApplication.getApplication(), request);
+        assertEquals(200, eventSearchResult.status());
+    }
+
+    @Then("^a event is displayed that I am attending$")
+    public void aEventIsDisplayedThatIAmAttending() throws Throwable {
+        EventFormData eventForm = new EventFormData();
+        eventForm.setAttending("on");
+        List<Events>  result = eventRepository.searchEvent(eventForm, 0);
+        assertEquals("BigEvent", result.get(0).getEventName());
+    }
+
+}
